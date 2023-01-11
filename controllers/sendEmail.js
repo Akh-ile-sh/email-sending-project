a6cc5406120fdda3cca07bc40a76d6ab1b93e9e9
Akhilesh C <akhileshcvdy@gmail.com>
2023-01-11 23:26:18 +0530

    initial commit

---
+++ controllers/sendEmail.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+const sendEmailEthereal = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "kellie97@ethereal.email",
+      pass: "95pgjDuaUSC1ck5hMD",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"user" <user@example.com>', // sender address
+    to: "bar@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  res.json(info);
+};
+
+const sendEmail = async (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "recepient@gmail.com", // Change to your recipient
+    from: "sender@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  const info = await sgMail.send(msg);
+  res.json(info);
+};
+
+module.exports = sendEmail;
